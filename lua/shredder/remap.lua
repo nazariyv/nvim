996da1f927d40e839ccb6e1382a36142f6d32860
nazariyv <hi@naz.ooo>
2023-01-15 01:30:06 +0000

    init commit

---
+++ lua/shredder/remap.lua
@@ -0,0 +1,44 @@
+-- allows you to move selection block
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+-- when moving through the page, the cursor stays in the middle
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+-- allows you to keep the searched term in the middle of the screen
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- if you have something like
+-- foo
+-- bar
+-- and you select and copy foo
+-- then select bar and paste in
+-- first one will be foo, but then if you
+-- press `p` again, it will be bar
+-- this keymap ensures that subsequent hits
+-- of `p` actually paste foo instead
+-- greatest remap ever according to the primeagen
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- next absolute greatest
+-- credit: asbjornHaland
+vim.keymap.set('n', '<leader>y', "\"+y")
+vim.keymap.set('v', '<leader>y', "\"+y")
+vim.keymap.set('n', '<leader>Y', "\"+Y")
+
+-- deleting into void register (doesn't get saved in buffer)
+vim.keymap.set('n', '<leader>dd', "\"_d")
+vim.keymap.set('v', '<leader>dd', "\"_d")
+
+vim.keymap.set('n', 'Q', '<nop>')
+vim.keymap.set('n', '<C-f>', '<cmd>silent !tmux neww tmux-sessionizer<CR>')
+
+-- lets you substitute all the occurences of a word you are on
+vim.keymap.set("n", "<leader>ss", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- when you indent selection, stay in visual mode
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
+
+-- press quickly jk to emulare Esc key
+vim.keymap.set('i', 'jk', '<ESC>')
