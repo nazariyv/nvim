d007be0d7a174aebfd26b52d7ebb2ccd7d323788
naz <hi@naz.ooo>
2024-01-14 15:44:20 +0000

    feat: gitsigna keymaps

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,23 +1,17 @@
 return {
-  {
-    "christoomey/vim-tmux-navigator",
-    lazy = false,
-  },
-  {
-    "numToStr/Comment.nvim",
-    config = function(_, opts)
-      require("Comment").setup(opts)
-    end,
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
-    end,
-  },
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    opts = {}, -- this is equivalent to setup({}) function
-  },
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+	},
+	{
+		"numToStr/Comment.nvim",
+		config = function(_, opts)
+			require("Comment").setup(opts)
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {}, -- this is equivalent to setup({}) function
+	},
 }
