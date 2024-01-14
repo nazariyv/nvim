b43fda377f8a5fb3c18386ba98f381ea849fe3a6
naz <hi@naz.ooo>
2024-01-14 15:30:20 +0000

    feat: vim options

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,18 +1,23 @@
 return {
-	{
-		"christoomey/vim-tmux-navigator",
-		lazy = false,
-	},
-	{
-		"numToStr/Comment.nvim",
-		config = function(_, opts)
-			require("Comment").setup(opts)
-		end,
-	},
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  {
+    "numToStr/Comment.nvim",
+    config = function(_, opts)
+      require("Comment").setup(opts)
+    end,
+  },
   {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup()
-    end
-  }
+    end,
+  },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {}, -- this is equivalent to setup({}) function
+  },
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,24 +1,59 @@
+local opt = vim.opt
+
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 
--- nvim-tree config: disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
--- set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
+opt.nu = true
+opt.relativenumber = true
+opt.wrap = false
+opt.swapfile = false
+opt.backup = false
+opt.hlsearch = false
+opt.incsearch = true
+opt.scrolloff = 8
+opt.signcolumn = "yes"
+opt.isfname:append("@-@")
+opt.updatetime = 50
+opt.colorcolumn = "80"
 
-vim.opt.nu = true
-vim.opt.relativenumber = true
-vim.opt.wrap = false
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-vim.opt.updatetime = 50
-vim.opt.colorcolumn = "80"
+opt.laststatus = 3 -- global statusline
+opt.showmode = false
+
+opt.clipboard = "unnamedplus"
+opt.cursorline = true
+
+-- Indenting
+-- opt.expandtab = true
+-- opt.shiftwidth = 2
+-- opt.smartindent = true
+-- opt.tabstop = 2
+-- opt.softtabstop = 2
+
+opt.fillchars = { eob = " " }
+opt.ignorecase = true
+opt.smartcase = true
+opt.mouse = "a"
+
+-- Numbers
+opt.number = true
+opt.numberwidth = 2
+opt.ruler = false
+
+-- disable nvim intro
+opt.shortmess:append("sI")
+
+opt.splitbelow = true
+opt.splitright = true
+opt.termguicolors = true
+opt.timeoutlen = 400
+opt.undofile = true
+
+-- interval for writing swap file to disk, also used by gitsigns
+opt.updatetime = 250
+
+-- go to previous/next line with h,l,left arrow and right arrow
+-- when cursor reaches end/beginning of line
+opt.whichwrap:append("<>[]hl")
