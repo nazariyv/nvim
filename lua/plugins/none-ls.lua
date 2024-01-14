be1ea2ff11c752d190f4df212f5af9b983564639
naz <hi@naz.ooo>
2024-01-14 15:38:37 +0000

    chore: reorg

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -2,13 +2,10 @@ return {
 	"nvimtools/none-ls.nvim",
 	config = function()
 		local null_ls = require("null-ls")
-
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
 			},
 		})
-
-		vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
 	end,
 }
