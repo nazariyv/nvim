a232846789be65af1a93e29757e77de991088d13
naz <hi@naz.ooo>
2024-01-14 15:53:12 +0000

    feat: null ls old config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,11 +1,20 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-			},
-		})
-	end,
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    local b = null_ls.builtins
+    null_ls.setup({
+      sources = {
+        b.formatting.stylua,
+        b.formatting.prettier.with({ filetypes = { "html", "markdown", "css" } }), -- so prettier works only on these filetypes
+        b.formatting.clang_format,
+        -- python static analysis and formatting
+        b.diagnostics.mypy,
+        b.diagnostics.ruff,
+        b.formatting.black.with({ filetypes = { "python" } }),
+        -- shellcheck
+        b.diagnostics.shellcheck,
+      },
+    })
+  end,
 }
