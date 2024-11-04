00c448372762ae5286fd6fcc024b72fab9451240
naz <hi@naz.ooo>
2024-11-04 00:48:43 +0000

    fix: rust-tools is no longer maintained

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,66 +1,59 @@
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 return {
-  -- poetry causes issues: https://github.com/christoomey/vim-tmux-navigator/issues/230
-  -- to have tmux navigator work nominally, rather than running poetry shell
-  -- and then nvim .
-  -- do: poetry run nvim
-  -- ! Note: this plugin requires tmux config as well
-  {
-    "christoomey/vim-tmux-navigator",
-    lazy = false,
-  },
-  {
-    "numToStr/Comment.nvim",
-    config = function(_, opts)
-      require("Comment").setup(opts)
-    end,
-  },
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    opts = {}, -- this is equivalent to setup({}) function
-  },
-  {
-    "rust-lang/rust.vim",
-    ft = "rust",
-    init = function()
-      vim.g.rustfmt_autosave = 1
-    end,
-  },
-  {
-    "simrat39/rust-tools.nvim",
-    ft = "rust",
-    dependencies = "neovim/nvim-lspconfig",
-    opts = {
-      server = {
-        -- on_attach = on_attach,
-        capabilities = capabilities,
-        -- ensuring it's starting mason managed rust-analyzer
-        -- to avoid duplicate diagnostics if you also have a system-wide installation
-        cmd = { vim.fn.stdpath("data") .. "/mason/bin/rust-analyzer"},
-      },
-    },
-    config = function(_, opts)
-      require("rust-tools").setup(opts)
-    end,
-  },
-  {
-    'saecki/crates.nvim',
-    ft = { 'rust', 'toml' },
-    config = function(_, opts)
-      local crates = require('crates')
-      crates.setup(opts)
-      crates.show()
-    end,
-  },
-  {
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
-  }
+	-- poetry causes issues: https://github.com/christoomey/vim-tmux-navigator/issues/230
+	-- to have tmux navigator work nominally, rather than running poetry shell
+	-- and then nvim .
+	-- do: poetry run nvim
+	-- ! Note: this plugin requires tmux config as well
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+	},
+	{
+		"numToStr/Comment.nvim",
+		config = function(_, opts)
+			require("Comment").setup(opts)
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {}, -- this is equivalent to setup({}) function
+	},
+	{
+		"rust-lang/rust.vim",
+		ft = "rust",
+		init = function()
+			vim.g.rustfmt_autosave = 1
+		end,
+	},
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^4",
+		lazy = false,
+		["rust-analyzer"] = {
+			cargo = {
+				allFeatures = true,
+			},
+		},
+	},
+	{
+		"saecki/crates.nvim",
+		ft = { "rust", "toml" },
+		config = function(_, opts)
+			local crates = require("crates")
+			crates.setup(opts)
+			crates.show()
+		end,
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
 }
